5424f509dd37be40eb2fcd7a1545a067f5e38114
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2024-11-05 19:40:17 +0700

    re init

--- src/store/useStore.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-const useStore = create((set) => ({
-  count: 0,
-  increaseCount: () => set((state) => ({ count: state.count + 1 })),
-  decreaseCount: () => set((state) => ({ count: state.count - 1 })),
-}));
-
-export default useStore;
